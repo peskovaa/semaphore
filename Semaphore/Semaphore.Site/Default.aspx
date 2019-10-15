121be8881206ead27c8c579d1acfa6dc9f2b9092
Alexander Peskov (DevExpress) <peskov.alexander@corp.devexpress.com>
2019-10-15 13:07:52 +0300

    Semaphore - Render

--- Semaphore/Semaphore.Site/Default.aspx
+++ Semaphore/Semaphore.Site/Default.aspx
@@ -7,6 +7,30 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <style>
+        .semaphore {
+            width: 100px;
+            height: 300px;
+            background: grey;
+            position: relative;    
+        }
+        .semaphore .circle {
+            width: 100px;
+            height: 100px;
+            border-radius: 50px;
+            position: relative;
+            background: red;
+        }
+        .semaphore .red-circle {
+            background: red;
+        }
+        .semaphore .yellow-circle {
+            background: yellow;
+        }
+        .semaphore .green-circle {
+            background: green;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
--- Semaphore/Semaphore.UI/Semaphore.cs
+++ Semaphore/Semaphore.UI/Semaphore.cs
@@ -1,12 +1,39 @@
 ﻿using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace Semaphore.UI {
     public class Semaphore : WebControl {
+        const string
+            SemaphorecssClassName = "semaphore",
+            CircleCssClassName = "circle",
+            RedCircleCssClassName = "red-circle",
+            YellowCircleCssClassName = "yellow-circle",
+            GreenCircleCssClassName = "green-circle";
+
+        public Semaphore()
+            : base(HtmlTextWriterTag.Div) {
+        }
 
         protected override void CreateChildControls() {
-            var literal = new LiteralControl("Hello from Semaphore!!!");
-            Controls.Add(literal);
+            if (!string.IsNullOrEmpty(CssClass))
+                CssClass += " ";
+            CssClass += SemaphorecssClassName;
+
+            var redCircle = CreateCirclye(RedCircleCssClassName);
+            Controls.Add(redCircle);
+
+            var yellowCircle = CreateCirclye(YellowCircleCssClassName);
+            Controls.Add(yellowCircle);
+
+            var greenCircle = CreateCirclye(GreenCircleCssClassName);
+            Controls.Add(greenCircle);
+        }
+
+        WebControl CreateCirclye(string coloredCircleCssClassName) {
+            var circle = new WebControl(HtmlTextWriterTag.Div);
+            circle.CssClass = $"{CircleCssClassName} {coloredCircleCssClassName}";
+            return circle;
         }
     }
 }
